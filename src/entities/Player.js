2e665775268f6500eb6be519565d0d783f8566c4
Fiox <123fiox1@gmail.com>
2026-01-23 22:37:32 +0100

    Add getter, attack(), change parent class, complete constructor, add list of word spell

--- src/entities/Player.js
+++ src/entities/Player.js
@@ -1,7 +1,43 @@
-import GameObject from "./GameObject.js";
+import Actor from "./Actor.js";
 
-export default class Player extends GameObject {
-  constructor(x, y) {
-    super(x, y);
+export default class Player extends Actor {
+  #wordSpells = [{ word: "undifined", damage: 100, range: 10 }];
+
+  constructor(
+    playerName = "Unknow",
+    hp = 100,
+    hpMax = 100,
+    position,
+    size,
+    img,
+  ) {
+    super(playerName, hp, hpMax, position, size, img);
+  }
+
+  //? closestEnemy = {target: Enemy, range: Number}
+  attack(closestEnemy, word) {
+    const spell = this.#wordSpells.find((wordSpell) => wordSpell.word === word);
+
+    if (!spell) {
+      throw new Error("There is no spell related to that word.");
+    }
+
+    if (closestEnemy.range <= spell.range) {
+      closestEnemy.target.hp = closestEnemy.target.hp - spell.damage;
+
+      return true;
+    }
+
+    return false;
+  }
+
+  get playerName() {
+    return this.playerName;
+  }
+  get hp() {
+    return this.hp;
+  }
+  get wordSpells() {
+    return this.#wordSpells.map((wordSpell) => wordSpell.word);
   }
 }
